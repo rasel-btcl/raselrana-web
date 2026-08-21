cdda4b9d8c4fa64657aba87a2ef04b1d1307023a
raselrana <raselrana.btcl@gmail.com>
2026-08-22 02:38:41 +0600

    The project structure created

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+// All component exports from this file for easier imports
+export { default as EmailForm } from "./EmailForm";
+export { default as SignalWave } from "./SignalWave";
---
+++ src/lib/mongodb.js
@@ -0,0 +1,1 @@
+// connection singleton (client/connect logic)
